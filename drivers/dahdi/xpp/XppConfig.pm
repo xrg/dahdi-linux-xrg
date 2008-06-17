9c91fe9c6260012df669f6211ce8557a89361136
tzafrir <tzafrir@c8ef98d6-144f-0410-837d-fce3034373d0>
2008-06-17 17:28:42 +0000

    Adapt xpp init_card_* scripts to dahdi: * No more dependency in zconf/ * Add new config file: xpp.conf * Use new perl module XppConfig.pm that read this config file.
    git-svn-id: http://svn.digium.com/svn/dahdi/linux/trunk@4373 c8ef98d6-144f-0410-837d-fce3034373d0

---
+++ drivers/dahdi/xpp/XppConfig.pm
@@ -0,0 +1,84 @@
+package XppConf;
+#
+# Written by Oron Peled <oron@actcom.co.il>
+# Copyright (C) 2008, Xorcom
+# This program is free software; you can redistribute and/or
+# modify it under the same terms as Perl itself.
+#
+# $Id$
+#
+use strict;
+
+my $conf_file = "/etc/dahdi/xpp.conf";
+
+$conf_file = $ENV{XPP_CONFIG} if $ENV{XPP_CONFIG};
+
+sub subst_var($$) {
+	my $lookup = shift;
+	my $string = shift;
+
+	if(defined $lookup->{$string}) {
+		return $lookup->{$string};
+	} else {
+		return $string;
+	}
+}
+
+sub read_config($) {
+	my $input = shift || die;
+	my %xpp_config;
+	my $lookup = \%xpp_config;
+
+	open(F, $input) || die "Failed reading configuration $input: $!\n";
+LINE:
+	while(<F>) {
+		chomp;
+		s/#.*//;	# strip comments
+		next unless /\S/;
+		s/^\s*//;
+		if(s/\\$//) {
+			my $next = <F>;
+			$next =~ s/^\s*//;
+			$_ .= " $next";
+			redo LINE;
+		}
+		my ($key, $value) = split(/=/, $_, 2);
+		# Trim whitespace around key/value
+		$key =~ s/^\s*(\S+)\s*$/$1/;
+		$value =~ s/^\s*(\S+)\s*$/$1/;
+		# Variable substitution
+		my $new_value = $value;
+		$new_value =~ s/\$(\w+)/subst_var($lookup,$1)/eg;
+		$xpp_config{$key} = $new_value;
+	}
+	close F;
+	return %xpp_config;
+}
+
+my %x = read_config($conf_file);
+
+sub show_vars {
+	my $assoc = shift;
+	foreach (sort keys %{$assoc}) {
+		print "$_\t$assoc->{$_}\n";
+	}
+}
+
+sub source_vars {
+	my @keys = @_;
+	my %conf = read_config($conf_file);
+	my %result;
+	my $k;
+	my $v;
+
+	foreach (@keys) {
+		if(defined $conf{$_}) {
+			$result{$_} = $conf{$_};
+		}
+	}
+	return ($conf_file, %result);
+}
+
+source_vars(qw(XPP_PRI_SETUP opermode));
+
+1;
